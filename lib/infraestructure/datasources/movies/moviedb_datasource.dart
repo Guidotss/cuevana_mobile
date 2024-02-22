c42f7046abcbcda614ddc72cfb1f6a221734ea3c
Guidotss <guidoolguin@hotmail.com>
2024-02-22 17:46:06 -0300

    terminando obtencion de peliculas populares

--- lib/infraestructure/datasources/movies/moviedb_datasource.dart
+++ lib/infraestructure/datasources/movies/moviedb_datasource.dart
@@ -1,12 +1,39 @@
-
+import 'package:dio/dio.dart';
+import 'package:guivana/config/constants/envs_variables.dart';
 import 'package:guivana/domain/datasources/movies/movies_datarsource.dart';
 import 'package:guivana/domain/entities/movie.dart';
+import 'package:guivana/infraestructure/mappers/movie_mapper.dart';
+import 'package:guivana/infraestructure/models/moviedb_response.dart';
 
 class MovieDbDataSource implements MovieDatasource {
-  @override
-  Future<List<Movie>> getMovies({ int page = 1 }) {
-    // TODO: implement getMovies
-    throw UnimplementedError();
+  final dio = Dio(BaseOptions(
+      baseUrl: 'https://api.themoviedb.org/3',
+      queryParameters: {
+        'api_key': EnvsVariables.theMovieDbKey,
+        'language': 'es-MX'
+      }));
+
+  List<Movie> _jsonToMovie(Map<String, dynamic> json) {
+    final movieDbResponse = MovieDbResponse.fromJson(json);
+    final List<Movie> movies = movieDbResponse.results
+        .where((movie) =>
+            movie.posterPath != 'no-poster' &&
+            movie.backdropPath != 'no-backdrop')
+        .map((movie) => MovieMapper.movieDbToEntity(movie))
+        .toList();
+
+    return movies;
   }
 
+  @override
+  Future<List<Movie>> getMovies({int page = 1}) async {
+    try {
+      final response =
+          await dio.get('/movie/popular', queryParameters: {'page': page});
+
+      return _jsonToMovie(response.data);
+    } catch (erorr) {
+      throw Exception('Error al obtener las peliculas');
+    }
+  }
 }
