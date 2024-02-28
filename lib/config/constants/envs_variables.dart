06685c09856e9a950ba422c1eba76ad77702d66e
Guidotss <guidoolguin@hotmail.com>
2024-02-28 18:23:17 -0300

    actualizando nombre de variable de entorno

--- lib/config/constants/envs_variables.dart
+++ lib/config/constants/envs_variables.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class EnvsVariables {
-  static String theMovieDbKey = dotenv.env['THE_MOVIE_DB_API_KEY'] ?? 'Not key found';
+  static String theMovieDbKey = dotenv.env['THE_MOVIEDB_KEY'] ?? 'Not key found';
 }
